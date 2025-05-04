29e46c225929f8b7b3ced80d2e86a3b104c5b94b
Mateo Perea Balvin <115613267+MateoBalvin@users.noreply.github.com>
2025-05-04 03:13:49 -0500

    practica sobre funciones con ejericios

--- Practica JavaScript/java.js
+++ Practica JavaScript/java.js
@@ -179,10 +179,248 @@ function ejemplo1_mayor(a3, b3) {
     return "el numero mayoe es el: " + b3;
   }
 }
-
 console.log(ejemplo1_mayor(10, 15));
 
-/**--PROMPT--*/ console.log("--PROMPT--");
+/*
+ Ejemplo #1 Funciones calculadora basica  console.log("--Ejemplo #1 Funciones sobre una Calculadora básica con dos números--");
+
+function sumar(s1, s2) {
+  return "La suma de estos numeros son: " + (s1 + s2);
+}
+
+function restar(s1, s2) {
+  return "La resta de estos numeros son: " + (s1 - s2);
+}
+
+function multiplicar(s1, s2) {
+  return "La multiplicacion de estos numeros son: " + s1 * s2;
+}
+
+function dividir(s1, s2) {
+  return "La division de estos numeros son: " + s1 / s2;
+}
+
+function main() {
+  console.log("");
+
+  let desicion = parseInt(
+    prompt(
+      "--Calculadora-- \n 1. Suma \n 2.Resta \n 3.Multiplicación \n 4.División \n5. Salir"
+    )
+  );
+
+  while (desicion != 5) {
+    switch (desicion) {
+      case 1:
+        s1 = parseInt(prompt("Ingresa el primer número: "));
+        s2 = parseInt(prompt("Ingresa el segundo número: "));
+
+        let mostrarsuma = sumar(s1, s2);
+
+        console.log(mostrarsuma);
+
+        break;
+
+      case 2:
+        s1 = parseInt(prompt("Ingresa el primer número: "));
+        s2 = parseInt(prompt("Ingresa el segundo número: "));
+
+        console.log(restar(s1, s2));
+
+        break;
+
+      case 3:
+        s1 = parseInt(prompt("Ingresa el primer número: "));
+        s2 = parseInt(prompt("Ingresa el segundo número: "));
+
+        console.log(multiplicar(s1, s2));
+        break;
+
+      case 4:
+        s1 = parseInt(prompt("Ingresa el primer número: "));
+        s2 = parseInt(prompt("Ingresaa el segundo número: "));
+
+        console.log(dividir(s1, s2));
+        break;
+    }
+    desicion=  parseInt(
+      prompt(
+        "--Calculadora-- \n 1. Suma \n 2.Resta \n 3.Multiplicación \n 4.División \n5. Salir"));
+  }
+}
+main();
+ 
+ */
+
+
+/*
+Ejemplo #2 Funciones Verificador de edad console.log("--Ejemplo #2 Funciones Verificador de edad--");
+
+  
+
+  function pediredad(){
+    let edad3 = parseInt(prompt("Bienvenido al BalvinClub \n¿Cual es tu edad?"));
+    return edad3;
+  }
+
+  function verificar_edad(edad3){
+
+    if(edad3<18){
+      return "No puedes entrar, eres menor de edad";
+    }
+    else if(edad3>=18 && edad3<60){
+
+      return "¡Bienvenido a la fiesta!";
+
+    }
+    else if(edad3>60){
+      return "Puedes entrar, pero cuidado con la música alta";
+
+    }
+  }
+21
+  function main2(){
+    let edad_usuario = pediredad();
+    
+    console.log(verificar_edad(edad_usuario));
+  }
+
+  main2();
+*/
+
+/*
+-- PROYECTO 1: PIEDAD-PAPEL-TIJERA--* console.log("--PROYECTO 1: PIEDAD-PAPEL-TIJERA--");
+
+function CpuChoise(){
+  let aleatorio = Math.floor(Math.random() * 3) + 1;
+  
+  switch(aleatorio){
+    case 1:
+      return "Piedra"
+      break;
+
+      case 2:
+        return "Papel"
+        break;
+
+      case 3:
+        return "Tijera"
+        break;
+  }
+  
+ }
+ 
+ function HumanChoise(){
+  let desicion1 = parseInt(prompt(" Elije sabiamente: \n 1. Piedra \n 2. Papel \n 3. Tijera"));
+  switch(desicion1){
+    case 1:
+      return "Piedra";
+      break;
+
+      case 2:
+        return "Papel";
+        break;
+
+      case 3:
+        return "Tijera";
+        break;
+  }  
+ }
+
+ let CpuScore =0;
+  let HumanScore =0;
+ 
+
+ function PlayRound(){
+  
+
+  let cpu = CpuChoise();
+  let human = HumanChoise();
+
+  //Piedra
+  if(human == "Piedra" && cpu == "Piedra"){
+    
+    return alert("EMPATE!! \n La CPU tambien escogio piedra... \n Fue un combate "+human+" vs "+cpu)
+  }
+
+  else if(human == "Piedra" && cpu == "Papel"){
+
+    CpuScore++;
+    return alert("PERDISTE!! \n La CPU escogio papel... \n Fue un combate "+human+" vs "+cpu)
+    
+  }
+
+  else if(human == "Piedra" && cpu == "Tijera"){
+
+    HumanScore++;
+    return alert("GANASTEE!! \n La CPU escogio tijera... \n Fue un combate "+human+" vs "+cpu)
+    
+  }
+
+  //Papel
+  else if(human == "Papel" && cpu == "Papel"){
+    
+    return "EMPATE!! \n La CPU tambien escogio papel... \n Fue un combate "+human+" vs "+cpu
+  }
+
+  else if(human == "Papel" && cpu == "Tijera"){
+    CpuScore++;
+    return "PERDISTE!! \n La CPU escogio tijera... \n Fue un combate "+human+" vs "+cpu
+    
+  }
+
+  else if(human == "Papel" && cpu == "Piedra"){
+    HumanScore++;
+    return "GANASTEE!! \n La CPU escogio Piedra... \n Fue un combate "+human+" vs "+cpu
+    
+  }
+
+  //Tijera
+  else if(human == "Tijera" && cpu == "Tijera"){
+    
+    return "EMPATE!! \n La CPU tambien escogio papel... \n Fue un combate "+human+" vs "+cpu
+  }
+
+  else if(human == "Tijera" && cpu == "Piedra"){
+    CpuScore++;
+    return "PERDISTE!! \n La CPU escogio piedra... \n Fue un combate "+human+" vs "+cpu
+    
+  }
+
+  else if(human == "Tijera" && cpu == "Papel"){
+
+    HumanScore++;
+    return "GANASTEE!! \n La CPU escogio papel... \n Fue un combate "+human+" vs "+cpu
+    
+  }
+
+  
+
+ }
+
+ function PlayGame(){
+  alert("Jugaras Piedra-Papel-Tijeras \n El juego tiene en total 5 RONDAS \n PREPARATEE!!");
+  let contador=0;
+
+
+  while(contador<5){
+    let ronda = PlayRound();
+    console.log(ronda);
+    contador++;
+  }
+  console.log("Esta es TÚ puntuacion de las veces que ganaste: "+HumanScore+" \n Esta es la puntuación de la CPU de las veces que te ganó: "+CpuScore);
+
+ }
+
+let jugada=PlayGame();
+console.log(jugada);
+
+*/
+  
+ 
+/*
+
+/**--PROMPT-- console.log("--PROMPT--");
 //Sirve para mostrar una ventana emergente al usuario y pedirle que ingrese un valor
 
 //Variable String
@@ -197,7 +435,7 @@ console.log(ejemplo1_mayor(10, 15));
   console.log("Tienes "+edad2+ " años");
   */
 
-/**EJEMPLOS PROMPT: */ console.log("--EJEMPLOS DE PROMPT--")
+/**EJEMPLOS PROMPT:  console.log("--EJEMPLOS DE PROMPT--")
 let numero_secreto = 7;
 let intentos = 0;
 let contador = 0;
@@ -216,4 +454,32 @@ while (contador != numero_secreto) {
 }
 intentos++;
 console.log("Felicidades, el numero que esta en mi cabezaa fue el " + numero_secreto+ " y lo hiciste con "+intentos+" intentos");
+
+*/
+
+/*-- ARRAYS --*/ console.log("-- ARRAYS --");
+//Una matriz un metodo permite que una variable contenga mas de un valor
+
+/*.Metodo #1 */ console.log(".Metodo #1");
+const carro1 = ["Renoult", "Mazda", "Toyota"];
+//alert(carro);
+console.log(carro1);
+
+/*.Metodo #2 */ console.log(".Metodo #2");
+const carro2=[];
+carro2[0]="Renoult";
+carro2[1]="Mazda";
+carro2[2]= "Toyota";
+
+console.log(carro2[1]);
+
+/*.Metodo #3 */ console.log(".Metodo #3: ");
+const carro3 = new Array("BMW","Audi",nombre);
+console.log(carro3[1],carro3[2]);
+
+let carro4 = carro3[0];
+console.log(carro4);
+
+/*.Metodo #4 */ console.log(".Metodo #4: ");
+
 
